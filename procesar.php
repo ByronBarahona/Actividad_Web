a138a417c0ace239a64fe77621eb1dd2a1bc9c2f
ByronBarahona <barahonabyron38@gmail.com>
2023-06-28 00:35:56 -0400

    Esta ok

---
+++ procesar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Boleta de compra</title>
+    <link href="Style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<body>
+    <div class="boleta">
+        <img src="../../Cine_Hoyts.png" alt="Logo cine" class="logo">
+        <h3>Boleta de compra</h3>
+        <?php
+        $descuentos = array(
+            'niño' => 0.3,
+            'adulto' => 0.05,
+            'adulto_mayor' => 0.55
+        );
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $edad = $_POST['edad'];
+            $cantidad = $_POST['cantidad'];
+
+            $descuento = $descuentos[$edad];
+            $subtotal = 20000 * $cantidad;
+            $descuento_aplicado = $subtotal * $descuento;
+            $total = $subtotal - $descuento_aplicado;
+
+            echo "<p>Entradas: $cantidad</p>";
+            echo "<p>Subtotal: $" . number_format($subtotal, 2) . "</p>";
+            echo "<p>Descuento ($edad): " . ($descuento * 100) . "%</p>";
+            echo "<p>Descuento aplicado: $" . number_format($descuento_aplicado, 2) . "</p>";
+            echo "<p>Total a pagar: $" . number_format($total, 2) . "</p>";
+        } else {
+            echo "<p>No se recibieron datos del formulario.</p>";
+        }
+        ?>
+    </div>
+</body>
+</html>
